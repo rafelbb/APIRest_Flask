2db668e89e637a3bfdb60b65d289651d92e99bdb
rafelbb <rafelbb@gmail.com>
2020-09-10 11:34:08 +0200

    Procfile para gunicorn y configuración producción

--- config/pro.py
+++ config/pro.py
@@ -1,21 +1,22 @@
 # config/pro.py
 
 from .default import *
-from os import environ, path
-from dotenv import load_dotenv
+from os import environ
+#from os import environ, path
+
+#from dotenv import load_dotenv
 
 
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+#basedir = path.abspath(path.dirname(__file__))
+#load_dotenv(path.join(basedir, '.env'))
 
 # Environment configuration
 APP_ENV = APP_ENV_PRODUCTION
 
-# Database configuration
-SQLALCHEMY_DATABASE_URI = 'postgresql://apirest:1234@localhost:5432/gestion'
+# Postgre heroku database url
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 SQLALCHEMY_ECHO = False
 
 
 SECRET_KEY = environ.get('SECRET_KEY')
-
